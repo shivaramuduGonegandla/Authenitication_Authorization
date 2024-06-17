5fd4bb15099771692192ed8bfa57f281f918be8a
shivaramudu <shivaramudu2002@gmail.com>
2024-06-17 17:28:34 +0530

    Auth?Auth

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from './Components/Home/Home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Home />,
+)
